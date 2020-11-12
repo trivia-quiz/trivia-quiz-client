b60bee0f984941260fff5ae32df904f4b497e7b5
wellytansil <we2y.tansil@gmail.com>
2020-11-13 05:48:51 +0700

    inserting twin-word API

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    questions: []
   },
   mutations: {
+    fetchQuestion (state, result) {
+      state.questions = result
+    }
   },
   actions: {
+    fetchQuestion (context) {
+      axios({
+        method: 'GET',
+        url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+        params: {level: '2', area: 'ms'},
+        headers: {
+        'x-rapidapi-key': 'ec68316836msh7a43a04bb39aae7p1c2164jsn910dc60d83da',
+        'x-rapidapi-host': 'twinword-word-association-quiz.p.rapidapi.com'
+        }
+      })
+      .then(result => {
+        context.commit('fetchQuestion', result.quizlist)
+      })
+    }
   },
   modules: {
   }
