4af42274acfdff53ac68b6f1e0ace4fb8b0889ba
wellytansil <we2y.tansil@gmail.com>
2020-11-13 06:16:41 +0700

    creating routes, fetching players

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,32 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+import axios from '../axios/axiosInstance'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    players: [],
     questions: []
   },
   mutations: {
-    fetchQuestion (state, result) {
+    fetchPlayers (state, result) {
+      state.players = result
+    },
+    fetchQuestions (state, result) {
       state.questions = result
     }
   },
   actions: {
-    fetchQuestion (context) {
+    fetchPlayers (context) {
+      axios({
+        method: 'GET',
+        url: 'http://localhost:3000/users'
+      })
+        .then(result => {
+          context.commit('fetchPlayers', result)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+
+    fetchQuestions (context) {
       axios({
         method: 'GET',
         url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
-        params: {level: '2', area: 'ms'},
+        params: { level: '2', area: 'ms' },
         headers: {
-        'x-rapidapi-key': 'ec68316836msh7a43a04bb39aae7p1c2164jsn910dc60d83da',
-        'x-rapidapi-host': 'twinword-word-association-quiz.p.rapidapi.com'
+          'x-rapidapi-key': 'ec68316836msh7a43a04bb39aae7p1c2164jsn910dc60d83da',
+          'x-rapidapi-host': 'twinword-word-association-quiz.p.rapidapi.com'
         }
       })
-      .then(result => {
-        context.commit('fetchQuestion', result.quizlist)
-      })
+        .then(result => {
+          context.commit('fetchQuestion', result.quizlist)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   },
   modules: {
