b0f1e439026b78b8c7b6f7d03103da599e78a9a8
jeplindm <jeplindm@github.com>
2020-11-13 10:04:58 +0700

    testing vue-socket.io

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000'
+}))
 
 new Vue({
   router,
